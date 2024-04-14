e317a1aa40640461d519ca21b1b4e4baa31cc8ae
Omkar Patil <113370674+OmkarPatil1117@users.noreply.github.com>
2024-04-14 08:10:43 -0700

    "2nd Qus Solved"

---
+++ index.js
@@ -0,0 +1,37 @@
+// Q Write a function to find longest that takes input a string and return thr longest word in thr string, if there are mulitple longest words, return the first encounterd ?
+
+// const str = `Hello My name Omkar Rudragouda Patil`;
+// const FindLongestWord = (str) => {
+//     if(!str) {
+//         return false
+//     }
+//     let WordsCount = ""
+//     const Words = str.split(" ");
+
+//     for(item of Words ) {
+//         if(WordsCount.length <= item.length) {
+//             WordsCount = item
+//         }
+//     }
+//     return WordsCount
+// }
+
+// console.log(FindLongestWord(str));
+
+
+// Q Write a function in JavaScript to find the maximum number in an array of integers.
+
+// const maximumNumber = (nums) => {
+//     let maximumNum = 0;
+//     for(num of nums) {
+//         if(maximumNum <= num) {
+//             maximumNum = num
+//         }
+//     }
+//     return maximumNum
+// }
+
+// console.log(maximumNumber([75, 10,20,30,65]))
+
+
+
